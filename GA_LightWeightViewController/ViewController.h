9cb3598f8b851c478431d7d67712ff03a39b135f
kongzichixiangjiao <58895347@qq.com>
2016-06-16 11:02:33 +0800

    Initial Commit

---
+++ GA_LightWeightViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GA_LightWeightViewController
+//
+//  Created by houjianan on 16/6/16.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GA_LightWeightViewControllerTests/GA_LightWeightViewControllerTests.m
@@ -0,0 +1,39 @@
+//
+//  GA_LightWeightViewControllerTests.m
+//  GA_LightWeightViewControllerTests
+//
+//  Created by houjianan on 16/6/16.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GA_LightWeightViewControllerTests : XCTestCase
+
+@end
+
+@implementation GA_LightWeightViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
